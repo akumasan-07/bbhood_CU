c0d53903398095212893358956eba2d4a0d13977
akumasan <ankurvr288@gmail.com>
2025-07-17 14:57:30 +0530

    add page student checkin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import StudentCheckin from './pages/StudentCheckin';
+import StudentDetails from './pages/StudentDetails';
+import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return <StudentCheckin />;
